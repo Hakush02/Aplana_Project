66eaa2d6b60448df4ebd342b2a97732544d585b9
Gayk <89253439263@mail.ru>
2019-04-08 23:18:18 +0300

    Second commit

--- src/Task_3/HelloWorld.java
+++ src/Task_3/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
             public static void main(String[] args) {
 
             System.out.println("Hello, world!");
-            String = "some new" ;
+            String additional = "some new string for 2d commit" ;
+
         }
     }
