97746ce4f1aef0a680b356b027b022ec9ca28652
Gayk <89253439263@mail.ru>
2019-04-08 22:52:30 +0300

    Initial commit

---
+++ src/Task_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner ;
+
+
+public class Task_1 {
+
+    public static void main (String [] args)   {
+
+        System.out.print("HelloHayk \n");
+        int x = 5 ;
+        int y = 6 ;
+        System.out.println( "x="+x+"; y="+y );
+        System.out.printf( "x=%d, y=%d \n" , x,y );
+        /*comments
+        comments comments */
+
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("Input a number: ") ;
+        int num = sc.nextInt() ;
+        System.out.printf("Your number: %d \n", num) ;
+
+
+    }
+
+}
---
+++ src/Task_3/HelloWorld.java
@@ -0,0 +1,10 @@
+package Task_3;
+
+public class HelloWorld {
+
+            public static void main(String[] args) {
+
+            System.out.println("Hello, world!");
+            String = "some new" ;
+        }
+    }
